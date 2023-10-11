2e5d5ea0b830180c0c07dc48729b2721f6258a43
cloudhand233-PC <cloudhand233@gmail.com>
2023-10-11 10:49:31 +0800

    add user privacy and services

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,43 @@ const app = getApp()
 
 Page({
   data: {
+    canStart: false
   },
   // 事件处理函数
   start_to_ques: function () {
+    if (this.data.canStart == true) {
+      wx.navigateTo({
+        url: '../ques1patient/ques1patient'
+      })
+    } else {
+      wx.showToast({
+        title: '请先阅读并同意用户协议及隐私政策',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+  },
+  //! 隐私协议及用户协议
+  user_privacy_page: function () {
     wx.navigateTo({
-      url: '../ques1patient/ques1patient'
+      url: '../user-privacy/user-privacy'
     })
+  },
+  user_services_page: function () {
+    wx.navigateTo({
+      url: '../user-services/user-services'
+    })
+  },
+  user_checked: function (e) {
+    if (e.detail.value == 'checked') {
+      this.setData({
+        canStart: true
+      })
+    } else {
+      this.setData({
+        canStart: false
+      })
+    }
   },
   onLoad() {
     setTimeout(() => {
